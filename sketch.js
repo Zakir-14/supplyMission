8d4b0b8c11ccc0a6dc207053d59a5fe8737f552f
Zakir-14 <71498578+Zakir-14@users.noreply.github.com>
2020-10-15 19:30:26 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,115 +1,86 @@
-var userPaddle, computerPaddle, computerScore, playerScore, gameState, ball,scoreSound, wall_hitSound, hitSound;
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground,groundIMG;
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
 
-function preload(){
-  // scoreSound = loadSound('score.mp3');
-  // wall_hitSound = loadSound('wall_hit.mp3');
-  // hitSound = loadSound('hit.mp3');
-}
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png");
+	packageIMG=loadImage("package.png");
+	groundIMG = loadImage("ground2.png");
+}	
 
-function setup() { 
+function setup() {
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	
+	/*
+	box1 = createSprite(330,655,150,20);
+	box1.shapeColor = "red";
+	box2 = createSprite(265,630,20,60);
+	box2.shapeColor = "red";
+	box3 = createSprite(395,630,20,60);
+	box3.shapeColor = "red";
+    */
+
+	packageSprite=createSprite(200, 0, 10,10);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
+	packageSprite.visible = false;
+	//packageSprite.debug = true;
+	packageSprite.velocityX = 4;
+
+	helicopterSprite=createSprite(0, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG);
+	helicopterSprite.scale=0.6;
+	helicopterSprite.velocityX = 4; 
+
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
+	groundSprite.addImage(groundIMG);
+	groundSprite.debug = true;
+
+
+	engine = Engine.create();
+	world = engine.world;
+	
+	helicopterSprite = Bodies.rectangle(0,200,10,10,{isStatic:true});
+	World.add(world, helicopterSprite);
+	
+	packageBody = Bodies.rectangle(	200, 200 , 200,200 , {restitution:0.5, isStatic:true});
+	World.add(world, packageBody);
+	
+	//box1 = Bodies.rectangle(330,655,150,20,{isStatic:true})
+	//World.add(world, box1);
+
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+ 	World.add(world, ground);
+
+
+	Engine.run(engine);
   
-createCanvas(400,400);
-
-//create a user paddle sprite
-userPaddle = createSprite(390,200,10,70);
-
-//create a computer paddle sprite
-computerPaddle = createSprite(10,200,10,70);
-
-//create the pong ball
-ball = createSprite(200,200,12,12);
-
-computerScore = 0;
-playerScore = 0;
-gameState = "serve";
 }
 
-function draw() {  
-  //fill the computer screen with white color
-  background("white");
-  edges = createEdgeSprites();
-  //display Scores
-  text(computerScore,170,20);
-  text(playerScore, 230,20);
 
-  //draw dotted lines
-  for (var i = 0; i < 400; i+=20) {
-     line(200,i,200,i+10);
-  }
+function draw() {
+  rectMode(CENTER);
+  background(230);
 
-  if (gameState === "serve") {
-    text("Press Space to Serve",150,180);
-  }
+if(keyDown(DOWN_ARROW)){
+	Matter.Body.setStatic(packageBody, false)
+	packageSprite.visible = true;
+}
 
-  if (gameState === "over") {
-    text("Game Over!",170,160);
-    text("Press 'R' to Restart",150,180);
-  }
-
-  if (keyDown("r")) {
-    gameState = "serve";
-    computerScore = 0;
-    playerScore = 0;
-  }
-
-
-  //give velocity to the ball when the user presses play
-  //assign random velocities later for fun
-  if (keyDown("space") && gameState == "serve") {
-    ball.velocityX = 5;
-    ball.velocityY = 5;
-    gameState = "play";
-  }
-
-  //make the userPaddle move with the mouse
-  userPaddle.y = World.mouseY;
-
-
-
-  //make the ball bounce off the user paddle
-  if(ball.isTouching(userPaddle)){
-    //hitSound.play();
-    ball.x = ball.x - 5;
-    ball.velocityX = -ball.velocityX;
-  }
-
-  //make the ball bounce off the computer paddle
-  if(ball.isTouching(computerPaddle)){
-    //hitSound.play();
-    ball.x = ball.x + 5;
-    ball.velocityX = -ball.velocityX;
-  }
-
-  //place the ball back in the centre if it crosses the screen
-  if(ball.x > 400 || ball.x < 0){
-    //scoreSound.play();
-
-  if (ball.x < 0) {
-      playerScore++;
-    }
-    else {
-      computerScore++;
-    }
-
-    ball.x = 200;
-    ball.y = 200;
-    ball.velocityX = 0;
-    ball.velocityY = 0;
-    gameState = "serve";
-
-    if (computerScore=== 5 || playerScore === 5){
-      gameState = "over";
-    }
-  }
-
-  //make the ball bounce off the top and bottom walls
-  if (ball.isTouching(edges[2]) || ball.isTouching(edges[3])) {
-    ball.bounceOff(edges[2]);
-    ball.bounceOff(edges[3]);
-   // wall_hitSound.play();
-  }
-
-  //add AI to the computer paddle so that it always hits the ball
-  computerPaddle.y = ball.y;
+  packageSprite.x= packageBody.position.x;
+  packageSprite.y= packageBody.position.y; 
   drawSprites();
+  textSize(20);
+ text("DROP THE PACKAGE IN THE BOX",300,100);
+ 
 }
+
+
+
+
